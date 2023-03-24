4130c797f0303356e3fe613aca2e90fc6e055221
EronAdemi <111318711+EronAdemi@users.noreply.github.com>
2023-03-24 13:56:04 +0100

    fix(about): Removed html contact form and added javascript with validation

---
+++ about.js
@@ -0,0 +1,40 @@
+function validateForm() {
+    let n = document.forms["myForm"]["fname"].value;
+    let l = document.forms["myForm"]["flastname"].value;
+    let e = document.forms["myForm"]["femail"].value;
+    let p = document.forms["myForm"]["fphone"].value;
+  
+      var valEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+      var valPhone = /^\d{10}$/; 
+    if (n == "") {
+      document.forms["myForm"]["fname"].style.borderColor = "red";
+      alert("Name must be filled out!");
+  
+      return false;
+    } else {
+      document.forms["myForm"]["fname"].style.borderColor = "black";
+    }
+    if (l == "") {
+      document.forms["myForm"]["flastname"].style.borderColor = "red";
+      alert("Last Name must be filled out!");
+      return false;
+    } else {
+      document.forms["myForm"]["flastname"].style.borderColor = "black";
+    }
+    if (e == "" || !valEmail.test(e)) {
+      document.forms["myForm"]["femail"].style.borderColor = "red";
+      alert("Your Email is invaild!");
+      return false;
+    } else {
+      document.forms["myForm"]["femail"].style.borderColor = "black";
+    }
+    if (p == "" || !valPhone.test(p)) {
+      document.forms["myForm"]["fphone"].style.borderColor = "red";
+      alert("invalid phone number!");
+      return false;
+    } else {
+      document.forms["myForm"]["fphone"].style.borderColor = "black";
+    }
+    return true;
+  }
+  
